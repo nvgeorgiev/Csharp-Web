c055f0af40285b4fd10a320d82f6f42c96ac8415
Nikolay Georgiev <nv_georgiev@abv.bg>
2020-01-21 19:58:37 +0200

    Initial HttpRequester project

---
+++ HttpRequester/HttpRequester/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace HttpRequester
+{
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public class StartUp
+    {
+        public static async Task Main()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://softuni.bg/");
+            string result = await response.Content.ReadAsStringAsync();
+            File.WriteAllText("index.html", result);
+        }
+    }
+}
