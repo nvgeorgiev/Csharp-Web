7207490fa6e4d6c2ec53aa9d5f75118dd110d013
Nikolay Georgiev <nv_georgiev@abv.bg>
2020-01-23 06:26:28 +0200

    Updater HttpRequester after the Http Protocol lecture

--- HttpRequester/HttpRequester/StartUp.cs
+++ HttpRequester/HttpRequester/StartUp.cs
@@ -2,12 +2,57 @@
 {
     using System;
     using System.IO;
+    using System.Net;
     using System.Net.Http;
+    using System.Net.Sockets;
+    using System.Text;
     using System.Threading.Tasks;
 
     public class StartUp
     {
         public static async Task Main()
+        {
+            const string NewLine = "\r\n";
+
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 80);
+            tcpListener.Start();
+
+            while (true)
+            {
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+                using (NetworkStream networkStream = tcpClient.GetStream())
+                {
+                    // TODO: Use buffer
+                    byte[] requestBytes = new byte[1000000];
+                    int bytesRead = networkStream.Read(requestBytes, 0, requestBytes.Length);
+                    string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
+
+                    string responseText = @"<form action='/Account/Login' method='post'>
+                                                    <input type=date name='date' />
+                                                    <input type=text name='username' />
+                                                    <input type=password name='password' />
+                                                    <input type=submit value='Login' />
+                                            </form>";
+
+                    string response = "HTTP/1.0 200 OK" + NewLine +
+                                      "Server: SoftUniServer/1.0" + NewLine +
+                                      "Content-Type: text/html" + NewLine +
+                                      "Content-Length: " + responseText.Length + NewLine +
+                                      NewLine +
+                                      responseText;
+
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+                    networkStream.Write(responseBytes, 0, responseBytes.Length);
+
+                    Console.WriteLine(request);
+                    Console.WriteLine(new string('=', 60));
+                }
+            }
+        }
+
+        public static async Task HttpRequest()
         {
             HttpClient client = new HttpClient();
             HttpResponseMessage response = await client.GetAsync("https://softuni.bg/");
