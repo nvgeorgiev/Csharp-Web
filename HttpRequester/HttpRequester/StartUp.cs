5426eed1b17d6cdf2c814d1abd5d327d73c91f87
Nikolay Georgiev <nv_georgiev@abv.bg>
2020-01-29 20:28:38 +0200

    HttpRequester updated after Asynchronous Processing lecture

--- HttpRequester/HttpRequester/StartUp.cs
+++ HttpRequester/HttpRequester/StartUp.cs
@@ -12,52 +12,50 @@
     {
         public static async Task Main()
         {
-            const string NewLine = "\r\n";
-
             TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 80);
             tcpListener.Start();
 
             while (true)
             {
-                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                Task.Run(() => ProcessClientAsync(tcpClient));
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            }
+        }
 
-                using (NetworkStream networkStream = tcpClient.GetStream())
-                {
-                    // TODO: Use buffer
-                    byte[] requestBytes = new byte[1000000];
-                    int bytesRead = networkStream.Read(requestBytes, 0, requestBytes.Length);
-                    string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
+        private static async Task ProcessClientAsync(TcpClient tcpClient)
+        {
+            const string NewLine = "\r\n";
 
-                    string responseText = @"<form action='/Account/Login' method='post'>
+            using (NetworkStream networkStream = tcpClient.GetStream())
+            {
+                // TODO: Use buffer
+                byte[] requestBytes = new byte[1000000];
+                int bytesRead = await networkStream.ReadAsync(requestBytes, 0, requestBytes.Length);
+                string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
+
+                string responseText = @"<form action='/Account/Login' method='post'>
                                                     <input type=date name='date' />
                                                     <input type=text name='username' />
                                                     <input type=password name='password' />
                                                     <input type=submit value='Login' />
-                                            </form>";
+                                            </form>" + "<h1>" + DateTime.UtcNow + "</h1>";
 
-                    string response = "HTTP/1.0 200 OK" + NewLine +
-                                      "Server: SoftUniServer/1.0" + NewLine +
-                                      "Content-Type: text/html" + NewLine +
-                                      "Content-Length: " + responseText.Length + NewLine +
-                                      NewLine +
-                                      responseText;
+                string response = "HTTP/1.0 200 OK" + NewLine +
+                                  "Server: SoftUniServer/1.0" + NewLine +
+                                  "Content-Type: text/html" + NewLine +
+                                  "Content-Length: " + responseText.Length + NewLine +
+                                  NewLine +
+                                  responseText;
 
-                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
 
-                    networkStream.Write(responseBytes, 0, responseBytes.Length);
+                await networkStream.WriteAsync(responseBytes, 0, responseBytes.Length);
 
-                    Console.WriteLine(request);
-                    Console.WriteLine(new string('=', 60));
-                }
+                Console.WriteLine(request);
+                Console.WriteLine(new string('=', 60));
             }
-        }
-
-        public static async Task HttpRequest()
-        {
-            HttpClient client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync("https://softuni.bg/");
-            string result = await response.Content.ReadAsStringAsync();
-            File.WriteAllText("index.html", result);
         }
     }
 }
