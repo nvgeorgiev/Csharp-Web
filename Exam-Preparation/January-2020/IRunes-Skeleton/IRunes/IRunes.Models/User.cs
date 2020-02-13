0d9280de3dac63376a82eaf4c1de993298d96b63
Nikolay Georgiev <nv_georgiev@abv.bg>
2020-02-13 16:04:20 +0200

    IRunes database created

--- Exam-Preparation/January-2020/IRunes-Skeleton/IRunes/IRunes.Models/User.cs
+++ Exam-Preparation/January-2020/IRunes-Skeleton/IRunes/IRunes.Models/User.cs
@@ -1,13 +1,26 @@
-﻿namespace IRunes.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IRunes.Models
 {
     public class User
     {
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Key]
         public string Id { get; set; }
 
+        [MaxLength(10)]
+        [Required]
         public string Username { get; set; }
 
+        [Required]
         public string Email { get; set; }
 
+        [Required]
         public string Password { get; set; }
     }
 }
