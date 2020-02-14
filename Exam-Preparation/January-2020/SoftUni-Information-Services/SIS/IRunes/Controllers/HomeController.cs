e69d14d860553ed033cc9554a0875bd0f735e7ba
Nikolay Georgiev <nikolaay.georgiev@gmail.com>
2020-02-14 09:16:19 +0200

    Added initial views of the pages

---
+++ Exam-Preparation/January-2020/SoftUni-Information-Services/SIS/IRunes/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using SIS.HTTP;
+using SIS.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRunes.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public HttpResponse Index() 
+        {
+            return this.View();
+        }
+
+        [HttpGet("/Home/Index")]
+        public HttpResponse IndexFullPage()
+        {
+            return this.Index();
+        }
+    }
+}
---
+++ Exam-Preparation/January-2020/SoftUni-Information-Services/SIS/IRunes/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using SIS.HTTP;
+using SIS.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRunes.Controllers
+{
+    public class UsersController : Controller
+    {
+        public HttpResponse Login()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public HttpResponse Login(string username, string password)
+        {
+
+        }
+
+        public HttpResponse Register()
+        {
+            return this.View();
+        }
+    }
+}
